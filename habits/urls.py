f49c18c9d5f2d2c9232a4c5a60272cccac0212da
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-06-27 16:13:37 +0300

    -Create CRUD views for habits.

---
+++ habits/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from habits import views
+from habits.apps import HabitsConfig
+
+app_name = HabitsConfig.name
+
+urlpatterns = [
+    path('habit/create/', views.HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habit/', views.HabitListAPIView.as_view(), name='habit-list'),
+    path('habit/<int:pk>/', views.HabitRetrieveAPIView.as_view(), name='habit-retrieve'),
+    path('habit/update/<int:pk>/', views.HabitUpdateAPIView.as_view(), name='habit-update'),
+    path('habit/delete/<int:pk>/', views.HabitDestroyAPIView.as_view(), name='habit-delete'),
+]
--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from habits.models import Habit
+
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    # permission_classes = (IsAuthenticated,)
+    serializer_class = HabitSerializer
+
+    # def perform_create(self, serializer):
+    #     habit = serializer.save()
+    #     habit.user = self.request.user
+    #     habit.save()
+
+
+class HabitListAPIView(generics.ListAPIView):
+    # permission_classes = (IsAuthenticated, IsOwner,)
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    # pagination_class = HabitPaginator
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    # permission_classes = (IsAuthenticated, IsOwner,)
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    # permission_classes = (IsAuthenticated, IsOwner,)
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    # permission_classes = (IsAuthenticated, IsOwner,)
+    queryset = Habit.objects.all()
