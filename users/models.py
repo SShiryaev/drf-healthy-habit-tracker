4c6f9948fbc15bdd289a524b7fb67bc04ad69962
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-06-27 13:53:14 +0300

    -Create User model.

--- users/models.py
+++ users/models.py
@@ -3,4 +3,22 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    """
+    Модель пользователя с наследованием от AbstractUser, исключением поля username
+    и переопределением USERNAME_FIELD на поле email.
+    """
+
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email')
+    avatar = models.ImageField(upload_to='users/', blank=True, null=True, verbose_name='аватар')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'Пользователь ({self.email})'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
