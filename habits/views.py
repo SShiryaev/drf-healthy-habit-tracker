f231f6310513bc8949893a422dac6da4674291c3
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-06-28 15:29:27 +0300

    -Add permissions for habit endpoints and authorization with simple-jwt.

--- habits/views.py
+++ habits/views.py
@@ -1,42 +1,46 @@
-from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from habits.paginators import HabitPaginator
 
 from habits.models import Habit
-
+from habits.paginators import HabitPaginator
 from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
 
 
 class HabitCreateAPIView(generics.CreateAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
     serializer_class = HabitSerializer
 
-    # def perform_create(self, serializer):
-    #     habit = serializer.save()
-    #     habit.user = self.request.user
-    #     habit.save()
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class HabitListAPIView(generics.ListAPIView):
-    # permission_classes = (IsAuthenticated, IsOwner,)
+    permission_classes = (IsAuthenticated, IsOwner,)
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
     pagination_class = HabitPaginator
 
 
+class HabitPublicListAPIView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
+    pagination_class = HabitPaginator
+
+
 class HabitRetrieveAPIView(generics.RetrieveAPIView):
-    # permission_classes = (IsAuthenticated, IsOwner,)
+    permission_classes = (IsAuthenticated, IsOwner,)
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
 
 
 class HabitUpdateAPIView(generics.UpdateAPIView):
-    # permission_classes = (IsAuthenticated, IsOwner,)
+    permission_classes = (IsAuthenticated, IsOwner,)
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
 
 
 class HabitDestroyAPIView(generics.DestroyAPIView):
-    # permission_classes = (IsAuthenticated, IsOwner,)
+    permission_classes = (IsAuthenticated, IsOwner,)
     queryset = Habit.objects.all()
