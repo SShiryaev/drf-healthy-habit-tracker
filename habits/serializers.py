9fd484e20be60a060b7ec4c0f82364f573c72ff0
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-06-30 18:55:08 +0300

    -Add and checking by flake8.

--- habits/serializers.py
+++ habits/serializers.py
@@ -12,8 +12,19 @@ class HabitSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('user',)
         validators = [
-            validators.DetachmentFieldsValidator(field_1='related_habit', field_2='reward'),
-            validators.DurationValidator(field='duration'),
-            validators.RelatedEnjoyableHabitValidator(field_1='related_habit'),
-            validators.EnjoyableHabitValidator(field_1='reward', field_2='related_habit', field_3='is_enjoyable'),
+            validators.DetachmentFieldsValidator(
+                field_1='related_habit',
+                field_2='reward'
+            ),
+            validators.DurationValidator(
+                field='duration'
+            ),
+            validators.RelatedEnjoyableHabitValidator(
+                field_1='related_habit'
+            ),
+            validators.EnjoyableHabitValidator(
+                field_1='reward',
+                field_2='related_habit',
+                field_3='is_enjoyable'
+            ),
         ]
